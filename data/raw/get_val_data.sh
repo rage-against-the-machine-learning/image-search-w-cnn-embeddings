7f335a74ee1535f91dd8e77bf8435ebf520679a4
godsylla <sylvi.tran@gmail.com>
2021-07-06 21:47:24 -0700

    reorg data download dir

--- data/raw/get_val_data.sh
+++
@@ -1,6 +0,0 @@
-# Validation Data
-mkdir /validation  && \
-cd /validation     && \
-curl http://images.cocodataset.org/zips/val2014.zip > val2014.zip && \
-unzip val2014.zip     && \
-rm val2014.zip
