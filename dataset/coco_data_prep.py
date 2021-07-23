98d04fb0bd9d098810afc7038a8eb5dda9b8f135
godsylla <sylvi.tran@gmail.com>
2021-07-22 21:30:19 -0700

    aws helper & s3 upload testing

--- dataset/coco_data_prep.py
+++ dataset/coco_data_prep.py
@@ -1,18 +1,35 @@
 import json
+import multiprocessing as mp
+import os
+import shutil
 import sys
 
 import albumentations as alb
 import cv2
 import numpy as np
+from tqdm import tqdm
 
-sys.path.append('../')
-import aws_helper
+sys.path.append('../src/')
+import aws_helper as awsh
 
 
-with open('./categories.json', 'r') as f:
+# GLOBAL VARIABLES ================================================= #
+data_split_to_use = 'train'
+with open('../dataset/categories.json', 'r') as f:
     categories_of_interest = json.load(f)
 
+# if 'api' is passed, then all images are retrieved from API
+# otherwise 'local' means all images will be fetched from local machine
+fetch_imgs_locally_or_api = 'local'
+local_img_directory = '../data/raw/coco/images/'
 
+num_cpus = 4
+batch_size = 250
+
+s3_bucket = 'cs7643-raml'
+s3_key_prefix = 'coco_train_np_imgs'
+
+# TRANSFORM FUNCTIONS TO RESIZE & NORMALIZE ======================= #
 def resize_and_pad_img(img: np.ndarray, img_size: int = 224):
     """
     :param img: image as np array (of either int or floats)
@@ -43,4 +60,57 @@ def normalize_img(img: np.ndarray):
     """
     return alb.Normalize(always_apply=True)(image=img).get('image')
 
+
+# WRAPPER MAIN FUNCTION ========================================== #
+def find(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
+
 def main():
+    # TODO: Write code to efficiently fetch from API
+    if fetch_imgs_locally_or_api == 'local':
+        print('Getting filepaths from local directory')
+        filenames = os.listdir(local_img_directory)
+        filepaths = [f"{local_img_directory}{fn}" for fn in tqdm(filenames)]
+
+    i = 0
+    while i < len(filenames):
+        print(f"images: {i} to {i + batch_size}")
+        print('Converting images to np arrays')
+        cv2_imgs = [cv2.imread(fp) for fp in filepaths[i: i + batch_size]]
+        np_images = [cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB) for cv2_img in tqdm(cv2_imgs)]
+
+        pool = mp.Pool(processes=num_cpus)
+        print('Resizing & padding images...')
+        resized = [pool.apply(resize_and_pad_img, args=([np_img])) for np_img in tqdm(np_images)]
+        print('Normalizing images...')
+        normed = [pool.apply(normalize_img, args=([res])) for res in tqdm(resized)]
+        pool.close()
+
+        print('Saving numpy images locally into temp directory...')
+        if not os.path.exists('./tmp/'):
+            os.makedirs('./tmp/')
+
+        filename_slice = filenames[i: i + batch_size]
+        np_filenames = [f"{fn.split('.')[0]}.np" for fn in filename_slice]
+        process_paths = [f"./tmp/{npfn}" for npfn in tqdm(np_filenames)]
+
+        for np_img, np_filepath in zip(normed, process_paths):
+            with open(np_filepath, 'wb') as p:
+                np.save(p, np_img, allow_pickle=True)
+
+        print('Writing files to s3 bucket...')
+        for path in tqdm(process_paths):
+            np_filename = path.split('/')[-1]
+            np_img = np.load(path)
+            s3_keypath = f"{s3_key_prefix}/{np_filename}"
+            awsh.upload_np_to_s3(s3_bucket, s3_keypath, np_img)
+
+        print('Resetting ./tmp/ directory')
+        shutil.rmtree('./tmp/')
+        i += batch_size
+
+
+if __name__ == "__main__":
+    main()
