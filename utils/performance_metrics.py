40b43528966153293f72e509d40f8a1a3ddfbff0
godsylla <sylvi.tran@gmail.com>
2021-08-02 21:17:33 +0000

    recall @ k annoy

--- utils/performance_metrics.py
+++ utils/performance_metrics.py
@@ -60,6 +60,47 @@ def annoy_precision (annoy_obj, query_idx: int, idx2coco_map: dict,
     return cat_precision_at_k, supercat_precision_at_k
 
 
+def annoy_recall (annoy_obj, query_idx: int, idx2coco_map: dict,
+                     n_neighbors: int = 10, at_k: int = 4):
+    
+    closest = annoy_obj.get_nns_by_item(query_idx, n_neighbors)
+    closest_at_k = closest[1: at_k + 1]
+    
+    cocoids = [idx2coco_map.get(idx) for idx in closest]
+    annids = [val_coco.getAnnIds(cid) for cid in cocoids]
+    
+    ground_truth_annots = val_coco.loadAnns(annids[0])
+    ground_truth_catids = [ann['category_id'] for ann in ground_truth_annots]  
+    ground_truth_label = category_id2name.get(Counter(ground_truth_catids).most_common()[0][0])
+    ground_truth_supercat = val_coco.cats.get(Counter(ground_truth_catids).most_common()[0][0])['supercategory']
+        
+    cat_labels = []
+    supercat_labels = []
+    
+    for an in annids:
+        cat_ids = []
+        
+        if len(an) > 0:
+            loaded_annots = [val_coco.loadAnns(a) for a in an]
+            cat_ids.append(Counter([l['category_id'] for loaded in loaded_annots for l in loaded]).most_common()[0][0])
+            supercat_labels.append([val_coco.cats.get(Counter([l['category_id'] for loaded in loaded_annots for l in loaded]).most_common()[0][0])['supercategory']][0])
+        else:
+            return None 
+        
+        cat_labels.extend([category_id2name.get(cid) for cid in cat_ids])
+    
+    cat_matches_at_k = np.sum([1 if l == ground_truth_label else 0 for l in cat_labels][1: at_k + 1])
+    supercat_matches_at_k = np.sum([1 if l == ground_truth_supercat else 0 for l in supercat_labels][1: at_k + 1])
+ 
+    cat_matches = np.sum([1 if l == ground_truth_label else 0 for l in cat_labels][1:])
+    supercat_matches = np.sum([1 if l == ground_truth_supercat else 0 for l in supercat_labels][1:])
+    
+    # turnery operator to ensure no division by 0 takes place
+    cat_recall_at_k = cat_matches / cat_matches if cat_matches != 0 else 0
+    supercat_recall_at_k = supercat_matches / supercat_matches if supercat_matches != 0 else 0
+        
+    return cat_recall_at_k, supercat_recall_at_k
+
 
 # NEAREST NEIGHBOR METRICS ========================================== #
 
